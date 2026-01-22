1ab529c378b622a8a76bbeb0efef3627849556ed
Javier Castillo <javiercame98@gmail.com>
2026-01-22 12:06:27 +0100

    Fix SQL syntax errors in queries

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,14 +1,14 @@
 -- 1. Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els/les alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
-SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo: `alumno` ORDER BY (apellido1, apellido2, nombre);
+SELECT nombre AS nom, apellido1 AS `primer cognom`, apellido2 AS `segon cognom` FROM persona WHERE tipo = 'alumno' ORDER BY (apellido1, apellido2, nombre);
 
 -- 2. Esbrina el nom i els dos cognoms dels alumnes que no han donat d'alta el seu número de telèfon en la base de dades. (nombre, apellido1, apellido2)
-SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo : `alumno` AND telefono IS NULL; 
+SELECT nombre, apellido1, apellido2 FROM persona WHERE tipo = 'alumno' AND telefono IS NULL; 
 
 -- 3. Retorna el llistat dels alumnes que van néixer en 1999. (id, nombre, apellido1, apellido2, fecha_nacimiento)
-SELECT id, nombre, apellido1, apellido2, fecha_nacimiento FROM persona WHERE tipo = `alumno` AND YEAR(fecha_nacimiento) = 1999;
+SELECT id, nombre, apellido1, apellido2, fecha_nacimiento FROM persona WHERE tipo = 'alumno' AND YEAR(fecha_nacimiento) = 1999;
 
 -- 4. Retorna el llistat de professors/es que no han donat d'alta el seu número de telèfon en la base de dades i a més el seu NIF acaba en K. (nombre, apellido1, apellido2, nif)
-SELECT nombre, apellido1, apellido2, nif FROM persona WHERE tipos `profesor` AND telefono IS NULL AND nif LIEK '%K';
+SELECT nombre, apellido1, apellido2, nif FROM persona WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE '%K';
 
 -- 5. Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7. (id, nombre, cuatrimestre, curso, id_grado)
 SELECT id, nombre, cuatrimestre, curso, id_grado FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND id_grado = 7;
